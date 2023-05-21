5e2daf57ee999155c113a00b30a8e49be6c88f2d
bilalcherkawi <cherkawibilal7@gmail.com>
2023-05-21 10:21:53 +0300

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const createDummyRows = () => {
+    let users = [];
+    for (let i = 0; i < 11; i++) {
+      let user = {
+        id: "id" + i,
+        firstName: `firstName ${i}`,
+        lastName: `lastName ${i}`,
+        dob: "2000-10-10",
+      };
+      users.push(user);
+    }
+    console.log(users);
+    return users;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>User id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {createDummyRows().map((value, index) => {
+            return (
+              <tr key={index}>
+                <td>{value.id}</td>
+                <td>{value.firstName}</td>
+                <td>{value.lastName}</td>
+                <td>{value.dob}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
